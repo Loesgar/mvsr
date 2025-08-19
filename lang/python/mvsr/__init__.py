6cedfe0ec667b07f526cc6f6d913436a7577aaf0
Bobbe <maxschlecht@web.de>
2025-08-19 14:25:52 +0200

    Use explicit reexports in __init__.py, add missing enums

--- lang/python/mvsr/__init__.py
+++ lang/python/mvsr/__init__.py
@@ -1,3 +1,9 @@
-from .mvsr import Kernel, Regression, segreg
+from .libmvsr import Algorithm as Algorithm
+from .libmvsr import Metric as Metric
+from .libmvsr import Score as Score
+from .mvsr import Interpolate as Interpolate
+from .mvsr import Kernel as Kernel
+from .mvsr import Regression as Regression
+from .mvsr import segreg as segreg
 
-__all__ = ["segreg", "Regression", "Kernel"]
+__all__ = ["Algorithm", "Metric", "Score", "Interpolate", "Kernel", "Regression", "segreg"]
